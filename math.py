30ae25e9e57ed5a0f1ccf4ccb5e04ad75dd43a9d
Nakakandebr <bridgetnakakandea@gmail.com>
2023-03-31 21:19:27 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    answer=a+b
+    return answer
+
+def subtract(x,y):
+    answer2=x-y
+    return answer2
+
+def multiply(t,p):
+    answer3=t*p
+    return answer3
+
+def remainder(f,l):
+    answer4=f%l
+    return answer4
+
